db66ddbbf8fd1c8bec9a566d307f78556c5d29fd
Carlen White <WhiterSuburban@gmail.com>
2022-01-09 06:20:15 -0500

    Seperate TimeDiffArg into seperate file

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -1,53 +1,9 @@
-use std::{path::PathBuf, str::FromStr, fmt::Display};
+use std::path::PathBuf;
 use chrono::Duration;
-
 use clap::Parser;
-use humantime::{parse_duration, format_duration};
 
-/// Tuple struct containing duration, used for arg parsing.
-#[derive(Debug, Clone, Copy)]
-pub(crate) struct TimeDiffArg(Duration);
-
-impl TimeDiffArg {
-    #[inline(always)]
-    fn duration(self) -> Duration {
-        self.0
-    }
-}
-
-impl FromStr for TimeDiffArg {
-    type Err = crate::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(Duration::from_std(parse_duration(s)?).unwrap()))
-    }
-}
-
-impl Into<std::time::Duration> for TimeDiffArg {
-    fn into(self) -> std::time::Duration {
-        self.0.to_std().unwrap()
-    }
-}
-
-impl From<Duration> for TimeDiffArg {
-    #[inline(always)]
-    fn from(d: Duration) -> Self {
-        Self(d)
-    }
-}
-
-impl Into<Duration> for TimeDiffArg {
-    #[inline(always)]
-    fn into(self) -> Duration {
-        self.duration()
-    }
-}
-
-impl Display for TimeDiffArg {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", format_duration(self.0.to_std().unwrap()))
-    }
-}
+mod time_diff_arg;
+use time_diff_arg::TimeDiffArg;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
